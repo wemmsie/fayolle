8ef71a3708bac3260eb797a3a6078d56b892125e
wemmsie <emily@thisjones.com>
2026-02-15 12:30:21 -0600

    scroll fix

--- src/main-address.jsx
+++ src/main-address.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import AddressPage from './AddressPage.jsx'
+import React, { useEffect } from 'react';
+import ReactDOM from 'react-dom/client';
+import AddressPage from './AddressPage.jsx';
+
+function AddressPageWithScroll() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  return <AddressPage />;
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AddressPage />
+    <AddressPageWithScroll />
   </React.StrictMode>,
 );
--- src/main-home.jsx
+++ src/main-home.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React, { useEffect } from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+
+function AppWithScroll() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  return <App />;
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AppWithScroll />
   </React.StrictMode>,
 );
