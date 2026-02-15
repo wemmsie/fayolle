4eed526bff0927c2eb95a152f9dad00fc57feba0
wemmsie <emily@thisjones.com>
2026-02-15 11:52:42 -0600

    update workflow

--- src/AddressPage.jsx
+++ src/AddressPage.jsx
@@ -1,23 +1,28 @@
 import './global.css'
 import * as image from './images'
 import { AddressForm } from './AddressForm'
-import { useEffect } from 'react';
+import { Helmet } from 'react-helmet-async';
 
 function AddressPage() {
-  useEffect(() => {
-    document.title = 'Address Please!';
-  }, []);
-
   return (
-    <div className='min-h-screen bg-pink'>
-      <section className='max my-10'>
-        <div className='form-container relative address-form'>
-          <img src={image.letter} alt='Love Letter' className='letter' />
-          <AddressForm />
-        </div>
-      </section>
-    </div>
-  )
+    <>
+      <Helmet>
+        <title>Send Your Address | Emily & Cody</title>
+        <meta property='og:title' content='Send Us Your Address!' />
+        <meta property='og:description' content='Help us send you something special 🩵' />
+        <meta property='og:image' content='/letter.png' />
+        <meta name='description' content='Help us send you something special 🩵' />
+      </Helmet>
+      <div className='min-h-screen bg-pink'>
+        <section className='max my-10'>
+          <div className='form-container relative address-form'>
+            <img src={image.letter} alt='Love Letter' className='letter' />
+            <AddressForm />
+          </div>
+        </section>
+      </div>
+    </>
+  );
 }
 
 export default AddressPage
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
 import App from './App.jsx'
 import AddressPage from './AddressPage.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<App />} />
-        <Route path='/address-please' element={<AddressPage />} />
-      </Routes>
-    </BrowserRouter>
+    <HelmetProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<App />} />
+          <Route path='/address-please' element={<AddressPage />} />
+        </Routes>
+      </BrowserRouter>
+    </HelmetProvider>
   </React.StrictMode>,
 );
