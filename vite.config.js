b3aa490cec12da9a87504bad0242cc420be0e3e9
wemmsie <emily@thisjones.com>
2026-02-15 12:07:50 -0600

    retry

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import prerender from 'vite-plugin-prerender';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [
