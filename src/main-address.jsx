ccd6fcd2869fea056d212f1dd49c261e31294870
wemmsie <emily@thisjones.com>
2026-02-15 12:28:30 -0600

    react router update and fix

---
+++ src/main-address.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import AddressPage from './AddressPage.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AddressPage />
+  </React.StrictMode>,
+);
