300805f533ad438e9604a83c66cb1fd0f0ee17d6
wemmsie <emily@thisjones.com>
2026-02-15 12:10:18 -0600

    update

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import prerender from 'vite-plugin-prerender';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    prerender({
-      staticDir: path.join(__dirname, 'dist'),
-      routes: ['/', '/address-please'],
-      renderer: '@prerenderer/renderer-puppeteer',
-      postProcess(renderedRoute) {
-        // Clean up routes
-        renderedRoute.html = renderedRoute.html.replace(/<script (.*?)>/gi, '<script $1 defer>');
-        return renderedRoute;
-      },
-    }),
-  ],
+  plugins: [react(), tailwindcss()],
 });
